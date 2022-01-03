52d268f6f9bfafa4f94db82a9ebb75d6a1c2555d
daryakuznetsova <daryamscw@gmail.com>
2022-01-03 17:15:02 +0300

    Add changes in home_work_1

---
+++ home_work_1/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+int[] CreateWholeNumbers(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int GetLength(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
+int[] CreateEvenNumbers(int[] array, int n)
+{
+    int[] newArray = new int[n];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    
+        
+    }
+    return newArray;
+}
+
+int[] wholeNumbersArray = CreateWholeNumbers(5);
+PrintArray(wholeNumbersArray);
+Console.WriteLine();
+PrintArray(CreateEvenNumbers(wholeNumbersArray, GetLength(wholeNumbersArray)));
